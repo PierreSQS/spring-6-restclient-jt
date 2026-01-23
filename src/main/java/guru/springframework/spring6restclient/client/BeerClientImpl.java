83171bfdde0f135ed0b1f5933f63b15d1e3204f9
PierreSQS <nomo_messina@web.de>
2026-01-23 14:05:28 +0100

    Sec43_Chap405: implemented update by ID

--- src/main/java/guru/springframework/spring6restclient/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/spring6restclient/client/BeerClientImpl.java
@@ -63,7 +63,15 @@ public class BeerClientImpl implements BeerClient {
 
     @Override
     public BeerDTO updateBeer(BeerDTO beerDto) {
-        return null;
+        RestClient restClient = restClientBuilder.build();
+
+        restClient.put()
+                .uri(GET_BEER_BY_ID_PATH, beerDto.getId().toString())
+                .body(beerDto)
+                .retrieve()
+                .body(BeerDTO.class);
+
+        return getBeerById(beerDto.getId());
     }
 
     @Override
