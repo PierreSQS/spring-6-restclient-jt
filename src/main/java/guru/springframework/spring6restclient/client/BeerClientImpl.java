1e9c714218d8c2c4f3323870c37759cfc569cdc9
PierreSQS <nomo_messina@web.de>
2026-01-24 11:47:22 +0100

    Sec43_Chap406: implemented delete by ID

--- src/main/java/guru/springframework/spring6restclient/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/spring6restclient/client/BeerClientImpl.java
@@ -76,6 +76,12 @@ public class BeerClientImpl implements BeerClient {
 
     @Override
     public void deleteBeer(UUID beerId) {
+        RestClient restClient = restClientBuilder.build();
+
+        restClient.delete()
+                .uri(GET_BEER_BY_ID_PATH, beerId.toString())
+                .retrieve()
+                .toBodilessEntity();
 
     }
 }
